85ea55862324102b0ae7ad9777b5d1cfb423a628
Ashutosh-at-079925877420 <ashutosh.d@girdmail.com>
2024-11-29 21:27:44 +0530

    first commit

---
+++ lib/teacher_form.dart
@@ -0,0 +1,12 @@
+// lib/teacher_form.dart
+import 'package:flutter/material.dart';
+
+class TeacherForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Teacher Form")),
+      body: Center(child: Text("This is the Teacher Form")),
+    );
+  }
+}
---
+++ test/teacher_form_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:teacher_form/teacher_form.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
